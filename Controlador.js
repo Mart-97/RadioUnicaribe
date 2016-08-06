def6c68c4ba1c11a68af470abfb3b4cd82f791b4
Pablo Melendez <pablofer.melendez@gmail.com>
2016-08-06 15:21:43 -0500

    se quitan encabezados de templates y se agregan paginas

--- Controlador.js
+++ Controlador.js
@@ -1,4 +1,28 @@
-$("#opc1").click( function(){
-   $("#main-wrapper").load("aux.html");
-    alert("Carga exitosa");
+ $("#news").click( function(){
+    //$("#main-wrapper").unload();
+    $("#main-wrapper").replaceWith($("#main-wrapper").load("noticias.html"));
+});
+$("#about").click( function(){
+    //$("#main-wrapper").unload();
+    $("main-wrapper").replaceWith($("#main-wrapper").load("acerca.html"));
+});
+$("#lastPrograms").click( function(){
+    //$("#main-wrapper").unload();
+    $("#main-wrapper").replaceWith($("#main-wrapper").load("pasados.html"));
+});
+$("#contact").click( function(){
+    //$("#main-wrapper").unload();
+    $("#main-wrapper").replaceWith($("#main-wrapper").load("contacto.html"));
+});
+$("#gallery").click( function(){
+    //$("#main-wrapper").unload();
+    $("#main-wrapper").replaceWith($("#main-wrapper").load("galeria.html"));
+});
+$("#programs").click( function(){
+    //$("#main-wrapper").unload();
+    $("#main-wrapper").replaceWith($("#main-wrapper").load("parrilla.html"));
+});
+$("#history").click( function(){
+    //$("#main-wrapper").unload();
+    $("#main-wrapper").replaceWith($("#main-wrapper").load("historia.html"));
 });
